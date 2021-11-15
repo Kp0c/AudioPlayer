0595ffd076501de9e33551b56ad118e2c6bfaacb
Kp0c <andriy.yushchenko97@gmail.com>
2021-11-15 16:02:59 +0200

    fix styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const CopyPlugin = require("copy-webpack-plugin");
-const path = require("path");
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  target: "web",
-  entry: "./src/js/index.js",
-  mode: "development",
+  target: 'web',
+  entry: './src/js/index.js',
+  mode: 'development',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {},
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "src/index.css", to: "." },
-        { from: "src/index.html", to: "." },
-        { from: "assets", to: "assets/" },
+        {from: 'src/index.css', to: '.'},
+        {from: 'src/index.html', to: '.'},
+        {from: 'assets', to: 'assets/'},
       ],
     }),
   ],
